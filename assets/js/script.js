150860a95de11cba80399967a7f5c28547c488d9
Stricker2 <101269390+Stricker2@users.noreply.github.com>
2022-06-10 23:58:20 -0400

    'Generate New Dog' button now randomly grabs dog pics from API upon click

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,20 @@
 
 // - create var elements
 var dogButtonEl = document.getElementById('dogbutton')
+var dogImage = document.getElementById('dog-image')
+var dogImageURL = 'https://dog.ceo/api/breeds/image/random'
 var duckButtonEl = document.getElementById('duckbutton')
 
 // - add button functionality
 dogButtonEl.addEventListener('click', function() {
-    console.log('dog button has been clicked');
+    fetch(dogImageURL).then(function(response) {
+        console.log(response);
+        response.json().then(function(data) {
+          console.log(data.message);
+          dogImage.src = data.message
+        });
+      });
+    
 })
 
 duckButtonEl.addEventListener('click', function() {
